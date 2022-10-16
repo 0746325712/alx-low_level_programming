d28c03161924ee1cbfbaebab70d398b55b97d509
0746325712 <mauricemasaga12@gmail.com>
2022-10-16 09:39:02 -0700

    100-intel

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+nclude <stdio.h>
+
+
+
+/**
+ *
+ * * main - Prints "with proper grammar, but the ouptu is a piece of art,"
+ *
+ * *
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
+int main(void)
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+}
