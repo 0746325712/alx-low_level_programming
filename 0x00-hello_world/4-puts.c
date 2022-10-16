eeb9a4a634367e3d21b2c10f11b2dd75f996e3d8
0746325712 <mauricemasaga12@gmail.com>
2022-10-16 10:15:46 -0700

    101-quote.c
    clear
    git commit -m 101-quote.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
-nclude <stdio.h>
+#include <stdio.h>
+
 /**
- *
  * * main - Write '"Programming is like building a multilingual puzzle'
  *
  * *Return: Always 0 (success)
  *
  * */
+
 int main(void)
+
+{
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
+}
